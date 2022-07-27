a364e1ccafd92ae180e6a6b5b2a892abc25a200a
DreamCode-Uz <javadeveloperbackend999@gmail.com>
2022-07-27 12:20:54 +0300

    Initial commit

---
+++ src/main/java/uz/pdp/springhotelapp/SpringHotelAppApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.springhotelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHotelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHotelAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/pdp/springhotelapp/SpringHotelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.springhotelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHotelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
