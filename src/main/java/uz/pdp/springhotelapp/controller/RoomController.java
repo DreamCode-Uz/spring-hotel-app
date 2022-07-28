e1755b68c8c01c96d552d62136bf82dc1b8aa315
DreamCode-Uz <javadeveloperbackend999@gmail.com>
2022-07-28 10:10:59 +0300

    update application

--- src/main/java/uz/pdp/springhotelapp/controller/RoomController.java
+++ src/main/java/uz/pdp/springhotelapp/controller/RoomController.java
@@ -1,9 +1,14 @@
 package uz.pdp.springhotelapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.pdp.springhotelapp.entity.Room;
+import uz.pdp.springhotelapp.payload.RoomDTO;
 import uz.pdp.springhotelapp.service.RoomService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/room")
@@ -14,5 +19,30 @@ public class RoomController {
     @Autowired
     public RoomController(RoomService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Room>> rooms() {
+        return service.getAll();
+    }
+
+    @GetMapping("/hotel/id={hotelId}")
+    public ResponseEntity<Page<Room>> getPageableRoom(@PathVariable("hotelId") Long id, @RequestParam("page") Integer page) {
+        return service.getParamPageableValue(id, page);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> save(@RequestBody RoomDTO dto) {
+        return service.saveRoom(dto);
+    }
+
+    @PutMapping("/id={roomId}")
+    public ResponseEntity<Object> update(@PathVariable("roomId") Long id, @RequestBody RoomDTO roomDTO) {
+        return service.updateRoom(id, roomDTO);
+    }
+
+    @DeleteMapping("/id={roomId}")
+    public ResponseEntity<String> delete(@PathVariable("roomId") Long id) {
+        return service.deleteRoom(id);
     }
 }
